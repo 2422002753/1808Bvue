bde322e8cade086f963a25bc072e8b0472acb562
HaoRan <2422002753@qq.com>
2019-07-09 19:16:51 +0800

    图片修改后提交

--- src/views/Car.vue
+++ src/views/Car.vue
@@ -1,3 +1,129 @@
 <template>
-	<div>购物车</div>
+  <div>
+    <van-nav-bar
+      title="详情"
+      left-text="返回"
+      right-text="分享"
+      left-arrow
+      @click-left="goback"
+      @click-right="share"
+    />
+    <van-checkbox-group class="card-goods" v-model="checkedGoods">
+      <van-checkbox class="card-goods__item" v-for="item in goods" :key="item.id" :name="item.id">
+        <van-card
+          :title="item.title"
+          :desc="item.desc"
+          :num="item.num"
+          :price="formatPrice(item.price)"
+          :thumb="item.thumb"
+        />
+      </van-checkbox>
+    </van-checkbox-group>
+    <van-submit-bar
+      :price="totalPrice"
+      :disabled="!checkedGoods.length"
+      :button-text="submitBarText"
+      @submit="onSubmit"
+    />
+  </div>
 </template>
+
+<script>
+import { Checkbox, CheckboxGroup, Card, SubmitBar, Toast } from "vant";
+export default {
+  components: {
+    [Card.name]: Card,
+    [Checkbox.name]: Checkbox,
+    [SubmitBar.name]: SubmitBar,
+    [CheckboxGroup.name]: CheckboxGroup
+  },
+  data() {
+    return {
+      checkedGoods: ["1", "2", "3"],
+      goods: [
+        {
+          id: "1",
+          title: "国产高山香甜自然熟香蕉",
+          desc: "约250g，2根",
+          price: 200,
+          num: 1,
+          thumb:
+            "https://img10.360buyimg.com/n1/jfs/t1/72896/31/3706/151809/5d1f22f2E6901c56d/72cbb9acb18aaaaf.jpg"
+        },
+        {
+          id: "2",
+          title: "新鲜酸草莓",
+          desc: "约600g",
+          price: 690,
+          num: 1,
+          thumb:
+            "https://img13.360buyimg.com/n1/jfs/t1/8261/6/5473/817476/5bde6ed3Efe0e1bed/6ab1529092f02982.jpg"
+        },
+        {
+          id: "3",
+          title: "烟台红富士苹果",
+          desc: "单果190-240g",
+          price: 2680,
+          num: 1,
+          thumb:
+            "https://img11.360buyimg.com/n1/jfs/t19927/93/671550299/292209/2d38f1e4/5b05395bNb9f0ee2f.jpg"
+        }
+      ]
+    };
+  },
+  computed: {
+    submitBarText() {
+      const count = this.checkedGoods.length;
+      return "结算" + (count ? `(${count})` : "");
+    },
+    totalPrice() {
+      return this.goods.reduce(
+        (total, item) =>
+          total + (this.checkedGoods.indexOf(item.id) !== -1 ? item.price : 0),
+        0
+      );
+    }
+  },
+  methods: {
+	   // 分享
+    share() {},
+    // 返回
+    goback() {
+      this.$router.push("/list1");
+    },
+    formatPrice(price) {
+      return (price / 100).toFixed(2);
+    },
+    onSubmit() {
+      Toast("点击结算");
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.card-goods {
+  padding: 10px 0;
+  background-color: #fff;
+  &__item {
+    position: relative;
+    background-color: #fafafa;
+    .van-checkbox__label {
+      width: 100%;
+      height: auto; // temp
+      padding: 0 10px 0 15px;
+      box-sizing: border-box;
+    }
+    .van-checkbox__icon {
+      top: 50%;
+      left: 10px;
+      z-index: 1;
+      position: absolute;
+      margin-top: -10px;
+    }
+    .van-card__price {
+      color: #f44;
+    }
+  }
+}
+</style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="goods">
-    <van-nav-bar title="详情"
-                 left-text="返回"
-                 right-text="分享"
-                 left-arrow
-                 @click-left="goback"
-                 @click-right="share" />
-    <van-swipe class="goods-swipe"
-               :autoplay="3000">
-      <van-swipe-item v-for="thumb in goods.thumb"
-                      :key="thumb">
-        <img :src="thumb">
+    <van-nav-bar
+      title="详情"
+      left-text="返回"
+      right-text="分享"
+      left-arrow
+      @click-left="goback"
+      @click-right="share"
+    />
+    <van-swipe class="goods-swipe" :autoplay="3000">
+      <van-swipe-item v-for="thumb in goods.thumb" :key="thumb">
+        <img :src="thumb" />
       </van-swipe-item>
     </van-swipe>
 
@@ -20,54 +20,34 @@
         <div class="goods-price">{{ formatPrice(goods.price) }}</div>
       </van-cell>
       <van-cell class="goods-express">
-        <van-col span="10">运费：
-          {{shopid}} | {{ goods.express }}</van-col>
+        <van-col span="10">
+          运费：
+          {{shopid}} | {{ goods.express }}
+        </van-col>
         <van-col span="14">剩余：{{ goods.remain }}</van-col>
       </van-cell>
     </van-cell-group>
 
     <van-cell-group class="goods-cell-group">
-      <van-cell value="进入店铺"
-                icon="shop-o"
-                is-link
-                @click="sorry">
+      <van-cell value="进入店铺" icon="shop-o" is-link @click="sorry">
         <template slot="title">
-          <span class="van-cell-text">阮野的店</span>
-          <van-tag class="goods-tag"
-                   type="danger">官方</van-tag>
+          <span class="van-cell-text">皓然的店</span>
+          <van-tag class="goods-tag" type="danger">官方</van-tag>
         </template>
       </van-cell>
-      <van-cell title="线下门店"
-                icon="location-o"
-                is-link
-                @click="sorry" />
+      <van-cell title="线下门店" icon="location-o" is-link @click="sorry" />
     </van-cell-group>
 
     <van-cell-group class="goods-cell-group">
-      <van-cell title="查看商品详情"
-                is-link
-                @click="sorry" />
+      <van-cell title="查看商品详情" is-link @click="sorry" />
     </van-cell-group>
 
     <van-goods-action>
-      <van-goods-action-icon icon="chat-o"
-                             @click="sorry">
-        客服
-      </van-goods-action-icon>
-      <van-goods-action-icon icon="cart-o"
-                             @click="onClickCart">
-        购物车
-      </van-goods-action-icon>
-      <van-goods-action-button type="warning"
-                               @click="sorry">
-        加入购物车
-      </van-goods-action-button>
-      <van-goods-action-button type="danger"
-                               @click="sorry">
-        立即购买
-      </van-goods-action-button>
+      <van-goods-action-icon icon="chat-o" @click="sorry">客服</van-goods-action-icon>
+      <van-goods-action-icon icon="cart-o" @click="onClickCart">购物车</van-goods-action-icon>
+      <van-goods-action-button type="warning" @click="sorry">加入购物车</van-goods-action-button>
+      <van-goods-action-button type="danger" @click="sorry">立即购买</van-goods-action-button>
     </van-goods-action>
-
   </div>
 </template>
 <script>
@@ -84,13 +64,13 @@ export default {
   data() {
     return {
       goods: {
-        title: "美国伽力果（约680g/3个）",
-        price: 2680,
+        title: "烟台红富士苹果（单果190-240g）",
+        price: 6880,
         express: "免运费",
         remain: 19,
         thumb: [
-          "https://img.yzcdn.cn/public_files/2017/10/24/e5a5a02309a41f9f5def56684808d9ae.jpeg",
-          "https://img.yzcdn.cn/public_files/2017/10/24/1791ba14088f9c2be8c610d0a6cc0f93.jpeg"
+          "https://img11.360buyimg.com/n1/jfs/t19927/93/671550299/292209/2d38f1e4/5b05395bNb9f0ee2f.jpg",
+          "https://img11.360buyimg.com/n1/jfs/t20950/277/233825024/271015/71e27b3f/5b05395aN7ba73be4.jpg"
         ]
       }
     };
@@ -100,7 +80,7 @@ export default {
     share() {},
     // 返回
     goback() {
-      this.$router.go(-1);
+      this.$router.push("/list1");
     },
     formatPrice() {
       return "¥" + (this.goods.price / 100).toFixed(2);
